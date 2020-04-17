506ca4d832e6066fe7222513f46e6e77ab4bec1b
Gabriel Engel <gfbengel@gmail.com>
2020-04-17 09:35:31 -0300

    projeto final

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,59 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
+}
+
+interface CreateTransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (total, transaction) =>
+        total + (transaction.type === 'income' ? transaction.value : 0),
+      0,
+    )
+    const outcome = this.transactions.reduce(
+      (total, transaction) =>
+        total + (transaction.type === 'outcome' ? transaction.value : 0),
+      0,
+    )
+    const total = income - outcome
+
+    return { income, outcome, total }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome') {
+      const { total } = this.getBalance()
+      if (value > total) {
+        throw Error(
+          "You don't have enough balance to execute this transaction.",
+        )
+      }
+    }
+
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
