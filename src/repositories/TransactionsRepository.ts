3d4fde8a22de8e6101f5d2c485001745d6bddb15
Gabriel Engel <gfbengel@gmail.com>
2020-04-17 10:00:04 -0300

    melhoria regra de negocio e getBalance()

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,16 +24,27 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const income = this.transactions.reduce(
-      (total, transaction) =>
-        total + (transaction.type === 'income' ? transaction.value : 0),
-      0,
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value
+            break
+          case 'outcome':
+            accumulator.outcome += transaction.value
+            break
+          default:
+            break
+        }
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
     )
-    const outcome = this.transactions.reduce(
-      (total, transaction) =>
-        total + (transaction.type === 'outcome' ? transaction.value : 0),
-      0,
-    )
+
     const total = income - outcome
 
     return { income, outcome, total }
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is invalid.')
+    }
+
     if (type === 'outcome') {
       const { total } = this.transactionsRepository.getBalance()
       if (value > total) {
